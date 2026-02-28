b6d106724b2b1d009006a3b6f5d03363d38db8ec
Amit Kumar Biswas <akbdeveloper01@gmail.com>
2026-02-28 22:49:25 +0530

    update

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+
+export const navLinks = [
+  { name: "Get Quote", href: '#' },
+  { name: "Request Hub", href: '#'},
+  { name: "Products&Services", href: '#' },
+  { name: "About us", href: '#' },
+]
